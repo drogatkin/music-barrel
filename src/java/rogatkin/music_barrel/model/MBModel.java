01ffff576615f9503941cfc1a342e68389351ce0
Dmitriy R <Dmitriy@metricstream.com>
2013-02-28 17:04:34 -0800

    added more build script including own

---
+++ src/java/rogatkin/music_barrel/model/MBModel.java
@@ -0,0 +1,12 @@
+package rogatkin.music_barrel.model;
+
+import com.beegman.webbee.model.AppModel;
+
+public class MBModel extends AppModel {
+	
+	@Override
+	public String getAppName() {
+		return "music-barrel";
+	}
+
+}
