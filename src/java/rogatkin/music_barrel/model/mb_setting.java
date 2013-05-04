76065bdb5e79bbfa853f269c90ad77831bac193e
Dmitriy R <Dmitriy@metricstream.com>
2013-05-04 15:03:30 -0700

    work around  djdk8 charsets bug

--- src/java/rogatkin/music_barrel/model/mb_setting.java
+++ src/java/rogatkin/music_barrel/model/mb_setting.java
@@ -7,15 +7,19 @@ import org.aldan3.annot.DataRelation;
 import org.aldan3.annot.FormField;
 import org.aldan3.annot.OptionMap;
 import org.aldan3.data.util.DataFiller;
+import org.aldan3.util.DataConv;
 import org.aldan3.data.util.FieldFiller;
 import com.beegman.webbee.util.DODelegatorEx;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Set;
+import java.util.HashSet;
 import java.util.TimeZone;
 import org.aldan3.data.util.FieldConverter;
+
 @DataRelation
 public class mb_setting extends SimpleCoordinator<MBModel> {
 	public enum output_type {
@@ -25,29 +29,29 @@ public class mb_setting extends SimpleCoordinator<MBModel> {
 	public mb_setting(MBModel arg0) {
 		super(arg0);
 	}
-	
-	@DBField(unique=true)
+
+	@DBField(unique = true)
 	public int id;
-	
+
 	@DBField()
 	public Date last_scan;
-	
+
 	@DBField()
-	@FormField(defaultTo="40", presentSize=3)
+	@FormField(defaultTo = "40", presentSize = 3)
 	public int page_size;
-	
-	@DBField(size=40)
-	@FormField(defaultTo="UTF-8", presentSize=16, presentFiller=CharsetsFiller.class)
+
+	@DBField(size = 40)
+	@FormField(defaultTo = "UTF-8", presentSize = 16, presentFiller = CharsetsFiller.class)
 	public String char_encoding;
-	
+
 	@DBField()
 	@FormField
 	public boolean perform_scan;
-	
+
 	@FormField
 	public boolean rescan_soon;
-	
-	@DBField(size=30, converter=AOEnumConv.class)
+
+	@DBField(size = 30, converter = AOEnumConv.class)
 	@FormField(presentFiller = EnumFiller.class)
 	public output_type output_type;
 
@@ -66,7 +70,7 @@ public class mb_setting extends SimpleCoordinator<MBModel> {
 							return principal.name();
 						return super.get(name);
 					}
-					
+
 					@Override
 					public boolean meanFieldFilter(String name) {
 						return "name".equals(name);
@@ -76,34 +80,38 @@ public class mb_setting extends SimpleCoordinator<MBModel> {
 			return result;
 		}
 	}
-	
+
 	public static class AOEnumConv implements FieldConverter<output_type> {
 		@Override
 		public output_type convert(String value, TimeZone tz, Locale l) {
 			return Enum.valueOf(output_type.class, value);
 		}
-		
+
 		@Override
 		public String deConvert(output_type value, TimeZone tz, Locale l) {
 			return value.toString();
 		}
 	}
-	
+
 	// TODO write utility class array filler 
 	public static class CharsetsFiller implements FieldFiller<DODelegatorEx[], mb_setting> {
 		public DODelegatorEx[] fill(mb_setting modelObject, String filter) {
-			Set<String> charsets =Charset.availableCharsets().keySet();
+			Set<String> charsets = null;
+			if ("1.8.0-ea-b36e".equals(System.getProperty("java.runtime.version")))
+				charsets = new HashSet<>(Arrays.asList(DataConv.HTML5_CHARSETS));
+			else
+				charsets = Charset.availableCharsets().keySet();
 			DODelegatorEx[] result = new DODelegatorEx[charsets.size()];
-			int i=0;
-			for(String is:charsets) {
-				result[i++] = new  DODelegatorEx<String>(is) {
+			int i = 0;
+			for (String is : charsets) {
+				result[i++] = new DODelegatorEx<String>(is) {
 					@Override
 					public Object get(String name) {
 						if ("name".equals(name) || "label".equals(name))
 							return principal;
 						return super.get(name);
 					}
-					
+
 					@Override
 					public boolean meanFieldFilter(String name) {
 						return "name".equals(name);
