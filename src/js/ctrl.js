b61f692bd75a2b4fc3072b697c3a5a7165d975c8
dmitriy <jaddressbook@gmail.com>
2022-04-16 13:18:17 -0700

    it looks like no extra encoding required for JS injected values

--- src/js/ctrl.js
+++ src/js/ctrl.js
@@ -70,3 +70,10 @@ function addAllFiles() {
          setTimeout (hidit, 500);
 	}
 }
+
+function htmlDecode(input){
+  var e = document.createElement('textarea');
+  e.innerHTML = input;
+  // handle case of empty input
+  return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
